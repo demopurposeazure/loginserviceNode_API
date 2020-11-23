d38caacf57809ddba8bfb46dde52620c7f48489a
demopurposeazure <demopurposeazure@tutanota.com>
2020-11-23 17:11:04 +0530

    Second Commit

--- routes/customUsers.js
+++ routes/customUsers.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.post('/', function(req, res, next) {
   console.log(req);
-  let response = {userName: 'admin',passWord:'admin',statusText:'OK',statusCode:200,reqbody:req.body,url:req.baseUrl};
+  let response = {userName: 'admin',passWord:'admin',statusText:'OK',statusCode:200,reqbody:req.body,url:req.baseUrl,original:req.originalUrl};
   res.json(response);
 });
 
